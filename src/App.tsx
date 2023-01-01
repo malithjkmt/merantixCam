2d46085772b3f31bb42a4745c217972b26d66dfb
Malith <malith@hatchyard.io>
2023-01-01 07:02:48 +0530

    Remove Sample Code and Setup Folder Structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+    </SafeAreaView>
+  );
+};
+
+export default App;
